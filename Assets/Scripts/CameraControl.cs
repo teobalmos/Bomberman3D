564f6d286f4a4405c96ea94c6d97aa8e7b6a1cbe
Teodora Balmoş <teo.balmos>
2020-02-18 18:36:29 +0000

    Add camera follow based on the number of players

--- Assets/Scripts/CameraControl.cs
+++ Assets/Scripts/CameraControl.cs
@@ -2,13 +2,15 @@
 using System.Collections.Generic;
 using UnityEngine;
 using System;
+using UnityEngine.EventSystems;
+using UnityEngine.UIElements;
 
 public class CameraControl : MonoBehaviour
 {
     public GameObject[] playerPrefab;
 
-    [SerializeField]
-    Transform target;
+//    [SerializeField]
+//    Transform target;
     [SerializeField]
     GameObject map;
     
@@ -19,15 +21,25 @@ public class CameraControl : MonoBehaviour
 
     private Vector3 offset;
     private List<Tuple<int, int>> spawnPositions = new List<Tuple<int, int>>();
-
-    void Start()
+    private Vector3 finalPosition;
+    private GameObject[] players;
+    private Camera camera;
+    private Vector3 moveVelocity;
+    private float zoomSpeed;
+    
+    private void Awake()
     {
-        SpawnPlayers();
-        //GenerateBlocks();
-        offset = transform.position - target.position;
+        camera = GetComponentInChildren<Camera>();
     }
 
-    void CreatePlayerSpawnPositions()
+    private void Start()
+    {
+        SpawnPlayers();
+        players = GameObject.FindGameObjectsWithTag("Player");
+//        offset = transform.position - target.position;
+    }
+
+    private void CreatePlayerSpawnPositions()
     {
         spawnPositions.Add(new Tuple<int, int>(-noOfRows, -noOfRows));
         spawnPositions.Add(new Tuple<int, int>(noOfRows, noOfRows));
@@ -35,11 +47,11 @@ public class CameraControl : MonoBehaviour
         spawnPositions.Add(new Tuple<int, int>(noOfRows, -noOfRows));
     }
 
-    void SpawnPlayers()
+    private void SpawnPlayers()
     {
         noOfPlayers = PlayerPrefs.GetInt("PlayerCount");
         CreatePlayerSpawnPositions();
-        for (int i = 0; i < noOfPlayers; i++)
+        for (var i = 0; i < noOfPlayers; i++)
         {
             GameObject player = playerPrefab[i];
             Debug.Log("i " + i + " // " + spawnPositions.Count);
@@ -48,8 +60,69 @@ public class CameraControl : MonoBehaviour
         }
     }
 
-    void FixedUpdate()
+    private void FindAveragePosition()
     {
-        transform.position = Vector3.Lerp(transform.position, target.position + offset, Time.deltaTime * 5);
+        var averagePos = new Vector3();
+        var noOfTargets = 0;
+
+
+        foreach (var player in players)
+        {
+            if(!player.gameObject.activeSelf)
+                continue;
+
+            averagePos += player.transform.position;
+            noOfTargets++;
+        }
+
+        if (noOfTargets > 0)
+            averagePos /= noOfTargets;
+
+        averagePos.y = transform.position.y;
+
+        finalPosition = averagePos;
+    }
+
+    private float FindRequiredSize()
+    {
+        var desiredLocalPos = transform.InverseTransformPoint(finalPosition);
+        var size = 0f;
+
+        foreach (var player in players)
+        {
+            if(!player.gameObject.activeSelf)
+                continue;
+            var targetLocalPos = transform.InverseTransformPoint(player.transform.position);
+            var posToTarget = targetLocalPos - desiredLocalPos;
+
+            size = Mathf.Max(size, Mathf.Abs(posToTarget.y));
+            size = Mathf.Max(size, Mathf.Abs(posToTarget.x / camera.aspect));
+            // edge buffer
+            size += 0.5f;
+            // size not too small
+            size = Mathf.Max(size, 6f);
+        }
+
+        return size;
+    }
+
+    private void Move()
+    {
+        FindAveragePosition();
+
+        transform.position = Vector3.SmoothDamp(transform.position, finalPosition, ref moveVelocity, 0.2f);
+    }
+
+    private void Zoom()
+    {
+        var requiredSize = FindRequiredSize();
+        camera.orthographicSize = Mathf.SmoothDamp(camera.orthographicSize, requiredSize, ref zoomSpeed, 0.2f);
+    }
+
+    private void FixedUpdate()
+    {
+        Move();
+        Zoom();
+//        transform.position = Vector3.Lerp(transform.position, target.position + offset, Time.deltaTime * 5);
     }
 }
